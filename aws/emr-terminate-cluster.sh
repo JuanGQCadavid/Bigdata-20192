fef29f375a36a0c94600a76f92fde5617bb9ca9d
Juan Gonzalo Quiroz Cadavid <jquiro12@eafit.edu.co>
2019-10-24 18:15:03 -0500

    Update emr-terminate-cluster.sh

--- aws/emr-terminate-cluster.sh
+++ aws/emr-terminate-cluster.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/bin/bash                                                                 
+
+if [ $(which jq) ]
+then
+    echo "Jq is installed"
+else
+    echo "No jq installed"
+    sudo apt-get install jq
+fi
 
 aws emr terminate-clusters --cluster-ids $(aws emr list-clusters --active | jq -r '.Clusters|.[0]|.Id')
